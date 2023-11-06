fff30810fa595f41d150e618a1d8af89a716cac9
RainBowT <rainbowt0506@gmail.com>
2023-11-06 13:43:12 +0800

    add annotations to your php files.
    command：php artisan l5-swagger:generate

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,15 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(title="L5 Swagger", version="1.0.0")
+ *
+ * @OA\Get(
+ *     path="/",
+ *     description="Controller",
+ *     @OA\Response(response="default", description="Controller")
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
